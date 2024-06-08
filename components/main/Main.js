0c1ba93ec3dba76757768af55bb05cd4ec7fb106
Shashiawari <109402358+Shashiawari@users.noreply.github.com>
2024-06-08 09:49:54 +0530

    Add files via upload

---
+++ components/main/Main.js
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import "./Main.css";
+import { motion } from "framer-motion";
+const Main = () => {
+  return (
+    <div className=" center ">
+      <div className="outmain ">
+        <div className="shashi">
+        <div className="loader">
+        <div className="face">
+            <div className="circle"></div>
+        </div>
+        <div className="face">
+            <div className="circle"></div>
+        </div>
+  
+     </div>
+        </div>
+        <div className="log d-flex">
+          <h1 className="text-center">Shashi Awari</h1>
+          <p className="text-center">I am a Web Developer And Programmer</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
